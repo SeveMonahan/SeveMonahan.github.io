8f2a252817a0d4cd449d0f7d360c2ab1d76bd316
SeveMonahan <monahan.seve+github1@gmail.com>
2022-05-15 21:52:48 +0000

    Finish studies/string-manipulation.js

---
+++ studies/string-manipulation.js
@@ -0,0 +1,27 @@
+/*
+* STRING MANIPULATION:
+*
+* 0. All strings are constants. Whenever you "manipulate" a string, you are technically creating
+* a new string based on one or more older strings.
+*
+* 1. Strings can be manipulated through either operators or built-in functions of javascript, each
+* of which do different things.
+*/
+
+// 1. With Operators //
+
+console.log("first half" + "second half"); //prints "first halfsecond half"
+
+// 2. With Functions //
+
+var pangram = "The quick brown fox jumps over the lazy dog.";
+
+// 'charAt' return the character (letter or digit) at a particular position 
+console.log(pangram.charAt(2)); // prints "e"
+
+// 'slice' slices part of the string
+console.log(pangram.slice(3)); // print " quick brown fox jumps over the lazy dog."
+console.log(pangram.slice(4,9)); // prints "quick"
+
+// There are dozens of other string manipulation functions. If you have a task to do,
+// somebody somewhere has probably written a function which can do it.
